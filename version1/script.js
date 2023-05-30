9c95de260630c16ea471e4072259200a933066a0
jenniCR <jenni.prime.illust@gmail.com>
2023-05-29 17:41:23 -0700

    version2

--- version1/script.js
+++ version1/script.js
@@ -2,11 +2,11 @@
   'use strict';
   console.log ('reading js')
 
-
+//the path 
   let path = anime.path('#demo-svg path')
-
+//the queryselector for the yes button
   const answerYes = document.querySelector('#yes');
-
+//for when they click it plays 
   answerYes.addEventListener('click', function(){
     var tl = anime.timeline({
       easing: 'easeOutExpo',
@@ -17,12 +17,12 @@
       targets: '.movingright',
       translateX: path('x'),
       translateY: path('y'),
-      delay: anime.stagger(400)
+      delay: anime.stagger(800)
     })
     .add({
       targets: '.movingleft',
       translateX: -1200,
-      delay: anime.stagger(400)
+      delay: anime.stagger(800)
     }, '-=3500') // relative offset
   })
   
